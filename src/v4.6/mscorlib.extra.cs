00128854a596db19d948103a2a89860109d44c17
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-03-23 00:18:53 +0100

    [v4.6] Manually add missing attributes and classes
    The attributes either reference internal types so GenAPI doesn't
    generate the referenced types or in case of StructLayout with
    LayoutKind=Auto GenAPI can't infer it.
    
    We also need to add a stub Mono.Cecil.dll to break the dependency
    in Mono.Debugger.Soft.dll

---
+++ src/v4.6/mscorlib.extra.cs
@@ -0,0 +1,27 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System
+{
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct ArgIterator { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct DateTime { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct DateTimeOffset { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct ValueTuple<T1, T2> { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct ValueTuple<T1, T2, T3> { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct ValueTuple<T1, T2, T3, T4> { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct ValueTuple<T1, T2, T3, T4, T5> { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct ValueTuple<T1, T2, T3, T4, T5, T6> { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct ValueTuple<T1, T2, T3, T4, T5, T6, T7> { }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)]
+    partial struct ValueTuple<T1, T2, T3, T4, T5, T6, T7, TRest> { }
+}
