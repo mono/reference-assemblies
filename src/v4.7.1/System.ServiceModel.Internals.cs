a27f0062cccb67800a3fb519fc70c79bccce57ce
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-09-08 02:38:38 +0200

    [v4.7.1] Add copy of 4.7 sources for 4.7.1 reference assemblies

---
+++ src/v4.7.1/System.ServiceModel.Internals.cs
@@ -0,0 +1,22 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+[assembly:System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
+[assembly:System.CLSCompliantAttribute(true)]
+[assembly:System.Diagnostics.DebuggableAttribute((System.Diagnostics.DebuggableAttribute.DebuggingModes)(2))]
+[assembly:System.Reflection.AssemblyDefaultAliasAttribute("System.ServiceModel.dll")]
+[assembly:System.Reflection.AssemblyDescriptionAttribute("Contains share code for some System.ServiceModel libraries")]
+[assembly:System.Reflection.AssemblyFileVersionAttribute("4.7.2046.0")]
+[assembly:System.Reflection.AssemblyInformationalVersionAttribute("4.7.2046.0")]
+[assembly:System.Reflection.AssemblyTitleAttribute("System.ServiceModel.Internals.dll")]
+[assembly:System.Resources.NeutralResourcesLanguageAttribute("en-US")]
+[assembly:System.Runtime.CompilerServices.CompilationRelaxationsAttribute(8)]
+[assembly:System.Runtime.CompilerServices.ReferenceAssemblyAttribute]
+[assembly:System.Runtime.CompilerServices.RuntimeCompatibilityAttribute(WrapNonExceptionThrows=true)]
+[assembly:System.Runtime.InteropServices.ComCompatibleVersionAttribute(1, 0, 3300, 0)]
+[assembly:System.Runtime.InteropServices.ComVisibleAttribute(false)]
+[assembly:System.Runtime.InteropServices.GuidAttribute("a9b8c4b5-b4a9-4800-8268-e8ec3b93d9ac")]
+[assembly:System.Security.AllowPartiallyTrustedCallersAttribute]
+[assembly:System.Security.SecurityCriticalAttribute((System.Security.SecurityCriticalScope)(0))]
+[assembly:System.Security.Permissions.SecurityPermissionAttribute(System.Security.Permissions.SecurityAction.RequestMinimum, SkipVerification=true)]
