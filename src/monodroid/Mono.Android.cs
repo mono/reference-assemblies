b986cb125a75ec74bc5281456278730a5d0c0b4b
EgorBo <egorbo@gmail.com>
2018-09-27 15:12:55 +0300

    remove comments

--- src/monodroid/Mono.Android.cs
+++ src/monodroid/Mono.Android.cs
@@ -1,9 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-// ------------------------------------------------------------------------------
-// Changes to this file must follow the http://aka.ms/api-review process.
-// ------------------------------------------------------------------------------
 
 [assembly:System.Runtime.CompilerServices.ReferenceAssemblyAttribute]
 [assembly:System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
