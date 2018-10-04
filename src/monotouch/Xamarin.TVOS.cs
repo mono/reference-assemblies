e331073688b36c42ac8ebfa0b0fd4c639f388e77
Marek Safar <marek.safar@gmail.com>
2018-10-04 14:32:14 +0200

    Adds XI product reference assemblies

---
+++ src/monotouch/Xamarin.TVOS.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+[assembly:System.Runtime.CompilerServices.ReferenceAssemblyAttribute]
+[assembly:System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
+[assembly:System.CLSCompliantAttribute(true)]
+
+namespace System.Drawing
+{
+    public struct Color {}
+    public enum KnownColor {}
+    public struct Point {}
+    public struct PointF {}
+    public struct Rectangle {}
+    public struct RectangleF {}
+    public struct Size {}
+    public struct SizeF {}
+}
